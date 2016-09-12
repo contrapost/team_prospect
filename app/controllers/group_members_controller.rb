462d964c368f92d523c12f33236441203b576ed2
Alexander Shipunov <shiale14@student.westerdals.no>
2016-09-12 23:31:05 +0200

    add redirection for admin page when CRUD users

--- app/controllers/group_members_controller.rb
+++ app/controllers/group_members_controller.rb
@@ -34,7 +34,7 @@ class GroupMembersController < ApplicationController
 
     respond_to do |format|
       if @group_member.save
-        format.html { redirect_to @group_member, notice: 'Group member was successfully created.' }
+        format.html { redirect_to admin_url, notice: 'Group member was successfully created.' }
         format.json { render :show, status: :created, location: @group_member }
       else
         format.html { render :new }
@@ -48,7 +48,7 @@ class GroupMembersController < ApplicationController
   def update
     respond_to do |format|
       if @group_member.update(group_member_params)
-        format.html { redirect_to @group_member, notice: 'Group member was successfully updated.' }
+        format.html { redirect_to admin_url, notice: 'Group member was successfully updated.' }
         format.json { render :show, status: :ok, location: @group_member }
       else
         format.html { render :edit }
@@ -62,7 +62,7 @@ class GroupMembersController < ApplicationController
   def destroy
     @group_member.destroy
     respond_to do |format|
-      format.html { redirect_to group_members_url, notice: 'Group member was successfully destroyed.' }
+      format.html { redirect_to admin_url, notice: 'Group member was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
