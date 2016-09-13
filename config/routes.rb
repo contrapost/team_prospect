78854e2e477c279102f977ca146d22d00110ecfe
Alexander Shipunov <shiale14@student.westerdals.no>
2016-09-13 16:12:49 +0200

    improve routing for admin login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :admins
   resources :group_members
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
@@ -7,8 +6,20 @@ Rails.application.routes.draw do
 
   get 'admin' => 'group_members#admin', as: :admin
 
-  devise_scope :admin do
-    get 'admin/logout' => 'devise/sessions#destroy'
-  end
+  # devise_scope :admin do
+  #   get 'admin/logout' => 'devise/sessions#destroy'
+  #   get 'admin/login' => 'devise/sessions#new'
+  #   post 'admin/login' =>  'devise/sessions#create'
+  # end
 
+  # devise_for :admin, path_names: {
+  #     sign_in: 'login', sign_out: 'logout'
+  # }
+
+  devise_for :admin, :skip => [:sessions, :passwords]
+  as :admin do
+    get 'admin/login' => 'devise/sessions#new', :as => :new_admin_session
+    post 'admin/login' => 'devise/sessions#create', :as => :admin_session
+    get 'admin/logout' => 'devise/sessions#destroy', :as => :destroy_admin_session
+  end
 end
