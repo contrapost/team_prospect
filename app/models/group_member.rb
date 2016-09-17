8cadc4469b23a043eef9b65432302f04026bd316
Alexander <shiale14@student.westerdals.no>
2016-09-17 13:03:00 +0200

    add photo attribute to news and set default picture for missing images

--- app/models/group_member.rb
+++ app/models/group_member.rb
@@ -1,6 +1,6 @@
 class GroupMember < ApplicationRecord
   has_many :links
 
-  has_attached_file :photo, styles: {medium: '300x300>', thumb: '100x100>'} #, default_url: '/images/:style/missing.png'
+  has_attached_file :photo, styles: {medium: '300x300>', thumb: '100x100>'}, default_url: 'https://s3.amazonaws.com/hovedprosjekt-shared/damaged.png'
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
 end
--- app/models/news.rb
+++ app/models/news.rb
@@ -1,2 +1,4 @@
 class News < ApplicationRecord
+  has_attached_file :photo, styles: {medium: '300x300>', thumb: '100x100>'}, default_url: 'https://s3.amazonaws.com/hovedprosjekt-shared/damaged.png'
+  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
 end
