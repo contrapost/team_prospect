93bef9ae0469c69a1f0f8f7f9d0039d15cfede52
Alexander Shipunov <shiale14@student.westerdals.no>
2016-09-26 21:02:24 +0200

    update news view with published/updated dates and fix bug in controller

--- app/controllers/news_paragraphs_controller.rb
+++ app/controllers/news_paragraphs_controller.rb
@@ -16,7 +16,7 @@ class NewsParagraphsController < ApplicationController
 
   def update
     news_paragraph = NewsParagraph.find(params[:news_paragraph_id])
-    news_paragraph.name = params[:paragraph]
+    news_paragraph.paragraph = params[:paragraph]
     news_paragraph.save
     respond_to do |format|
       format.html { redirect_to news_paragraphs_show_path(news_id: params[:news_id]), notice: 'Paragraph was successfully updated.' }
